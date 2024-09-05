ff5ac07bd5fd136858da896603841fe0de301fdf
Jeff Korenstein <jeffrey.korenstein@va.gov>
2024-09-05 14:15:04 -0400

    Workitem VBMSD-000000 remove book and author from BootStrapData

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,12 +1,9 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-import guru.springframework.spring5webapp.domain.Author;
+
 import guru.springframework.spring5webapp.domain.Beneficiary;
-import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.domain.Payment;
 import guru.springframework.spring5webapp.domain.Veteran;
-import guru.springframework.spring5webapp.repositories.AuthorRepository;
-import guru.springframework.spring5webapp.repositories.BookRepository;
 import guru.springframework.spring5webapp.repositories.VeteranRepository;
 import guru.springframework.spring5webapp.repositories.BeneficiaryRepository;
 import guru.springframework.spring5webapp.repositories.PaymentRepository;
@@ -20,17 +17,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class BootStrapData implements CommandLineRunner {
 
-    private final AuthorRepository authorRepository;
-    private final BookRepository bookRepository;
+//    private final AuthorRepository authorRepository;
+//    private final BookRepository bookRepository;
     private final VeteranRepository veteranRepository;
     private final BeneficiaryRepository beneficiaryRepository;
     private final PaymentRepository paymentRepository;
     private final ReturnedPaymentRepository returnedPaymentRepository;
 
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, VeteranRepository veteranRepository, BeneficiaryRepository beneficiaryRepository, PaymentRepository paymentRepository, ReturnedPaymentRepository returnedPaymentRepository) {
-        this.authorRepository = authorRepository;
-        this.bookRepository = bookRepository;
+    public BootStrapData(VeteranRepository veteranRepository, BeneficiaryRepository beneficiaryRepository, PaymentRepository paymentRepository, ReturnedPaymentRepository returnedPaymentRepository) {
         this.veteranRepository = veteranRepository;
         this.beneficiaryRepository = beneficiaryRepository;
         this.paymentRepository = paymentRepository;
@@ -39,26 +34,6 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "123123");
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
-
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
-        Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
-        rod.getBooks().add(noEJB);
-        noEJB.getAuthors().add(rod);
-
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
-
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Books: " + bookRepository.count());
-
         Veteran gloriaMendoza = new Veteran("Gloria Mendoza", "886532164");
         Beneficiary gloriaSpouse = new Beneficiary("John Mendoza", "01");
         Payment gloriaSpousePayment01 = new Payment("Regular Recurring", 1038.79F, "04/13/2018", "04/13/2018", "successful");
@@ -77,5 +52,7 @@ public class BootStrapData implements CommandLineRunner {
         System.out.println("number of veterans: " + veteranRepository.count());
         System.out.println("number of beneficiaries: " + beneficiaryRepository.count());
         System.out.println("number of payments: " + paymentRepository.count());
+        System.out.println(gloriaMendoza.toString());
+        System.out.println(gloriaSpouse.toString());
     }
 }
